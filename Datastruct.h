3d230c95ef8c3daa113d3e7bcda8973f57c938b9
ben <3050760440@qq.com>
2021-09-28 22:12:23 +0800

    添加项目文件。

---
+++ Datastruct.h
@@ -0,0 +1,35 @@
+#pragma once
+#include<stddef.h>
+#include<errno.h>
+#include<stdio.h>
+#include<math.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<vector>
+#include<list>
+#include<map>
+#include<deque>
+#include<set>
+#include<stack>
+#include <string>
+#include <iostream>¡¡
+#include <algorithm>
+
+#pragma once
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+
+};
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode* next) : val(x), next(next) {}
+
+};
