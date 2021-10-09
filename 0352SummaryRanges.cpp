0e7350bd26081d70d67cd34205087ccc3ce205d4
ben <3050760440@qq.com>
2021-10-09 21:48:41 +0800

    10-9 352 notdone

---
+++ 0352SummaryRanges.cpp
@@ -0,0 +1,18 @@
+#include"Datastruct.h"
+using namespace std;
+class SummaryRanges {
+private:
+	map<int, int> m_map;
+public:
+	SummaryRanges() {
+	}
+
+	void addNum(int val) {
+		auto right = m_map.upper_bound(val);
+		auto left = right == m_map.end() ?
+	}
+
+	vector<vector<int>> getIntervals() {
+
+	}
+};
