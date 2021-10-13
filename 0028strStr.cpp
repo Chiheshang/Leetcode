1b05c6a179e3a4910dd0876ae189778de02f8f25
ben <3050760440@qq.com>
2021-10-13 22:59:40 +0800

    kmp

---
+++ 0028strStr.cpp
@@ -0,0 +1,76 @@
+#include"Datastruct.h"
+using namespace std;
+class Solution {
+public:
+	int strStr(string haystack, string needle) {
+		return haystack.find(needle);
+	}
+    int strStrKmp(string haystack, string needle) {
+        int n = haystack.size(), m = needle.size();
+        if (m == 0) {
+            return 0;
+        }
+        vector<int> next(m);
+        for (int i = 1, j = 0; i < m; i++) {
+            while (j > 0 && needle[i] != needle[j]) {
+                j = next[j - 1];
+            }
+            if (needle[i] == needle[j]) {
+                j++;
+            }
+            next[i] = j;
+        }
+        for (int i = 0, j = 0; i < n; i++) {
+            while (j > 0 && haystack[i] != needle[j]) {
+                j = next[j - 1];
+            }
+            if (haystack[i] == needle[j]) {
+                j++;
+            }
+            if (j == m) {
+                return i - m + 1;
+            }
+        }
+        return -1;
+    }
+};
+string stringToString(string input) {
+    string result;
+    for (int i = 1; i < input.length() - 1; i++) {
+        char currentChar = input[i];
+        if (input[i] == '\\') {
+            char nextChar = input[i + 1];
+            switch (nextChar) {
+            case '\"': result.push_back('\"'); break;
+            case '/': result.push_back('/'); break;
+            case '\\': result.push_back('\\'); break;
+            case 'b': result.push_back('\b'); break;
+            case 'f': result.push_back('\f'); break;
+            case 'r': result.push_back('\r'); break;
+            case 'n': result.push_back('\n'); break;
+            case 't': result.push_back('\t'); break;
+            default: break;
+            }
+            i++;
+        }
+        else {
+            result.push_back(currentChar);
+        }
+    }
+    return result;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        string haystack = stringToString(line);
+        getline(cin, line);
+        string needle = stringToString(line);
+
+        int ret = Solution().strStrKmp(haystack, needle);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
