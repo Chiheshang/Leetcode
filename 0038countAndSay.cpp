c012f55d6c92e040d6299f2c2f5d425d4d3e36df
ben <3050760440@qq.com>
2021-10-18 11:40:19 +0800

    10-28

---
+++ 0038countAndSay.cpp
@@ -0,0 +1,8 @@
+#include"Datastruct.h"
+using namespace std;
+class Solution {
+public:
+    string countAndSay(int n) {
+
+    }
+};
--- 0058lengthOfLastWord.cpp
+++ 0058lengthOfLastWord.cpp
@@ -7,3 +7,20 @@ public:
 		return s.size() - s.find_last_of(" ");
 	}
 };
+struct item
+{
+public:
+	int i ;
+	char c ;
+	item(int i, char c) {
+		this->i = i;
+		this->c = c;
+	}
+};
+int main() {
+	item* array[3];
+	for (int i = 0; i < 3; i++) {
+		array[i] = new item(0, '3');
+	}
+	cout << array[0]->c << endl;
+}
