be84eb61e6102da56b2a6d3af2fee1b457c61cf3
ben <3050760440@qq.com>
2021-11-04 22:00:50 +0800

    11-4 141

---
+++ 0141hasCycle.cpp
@@ -0,0 +1,18 @@
+#include"Datastruct.h"
+using namespace std;
+class Solution {
+public:
+	bool hasCycle(ListNode* head) {
+		if (head == nullptr || head->next == nullptr)
+			return false;
+		ListNode* slow = head;
+		ListNode* fast = head->next;
+		while (slow != fast) {
+			if (fast == nullptr || fast->next == nullptr)
+				return false;
+			slow = slow->next;
+			fast = fast->next->next;
+		}
+		return true;
+	}
+};
