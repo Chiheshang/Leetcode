e4f2f5b57d2fcb8f6c3694fc178ef1bc8efc4d8b
ben <3050760440@qq.com>
2021-12-06 22:37:27 +0800

    12-6

---
+++ 1005largestSumAfterKNegations.cpp
@@ -0,0 +1,33 @@
+#include"Datastruct.h"
+using namespace std;
+class Solution {
+public:
+	static const bool compare(int a, int b) {
+		return abs(a) < abs(b);
+	}
+	int largestSumAfterKNegations(vector<int>& nums, int k) {
+		sort(nums.begin(), nums.end(), compare);
+		for (int i = nums.size() - 1; i >= 0 && k > 0; i--) {
+			if (nums[i] < 0) {
+				nums[i] *= -1;
+				k--;
+			}
+		}
+		k = k % 2;
+		if (k || !nums[0]) {
+			for (int i = 0; i < nums.size(); i++) {
+				if (nums[i] > 0) {
+					nums[i] *= -1;
+					break;
+				}
+			}
+		}
+		int sum = 0;
+		for_each(nums.begin(), nums.end(), [&sum](int cur) {sum += cur; });
+		return sum;
+	}
+};
+int main() {
+	vector<int > a = { 2, -3, -1, 5, -4 };
+	Solution().largestSumAfterKNegations(a, 2);
+}
