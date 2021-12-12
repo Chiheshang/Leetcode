14a5d239a1e99a9c0c36afdfa2509a98c31048ce
ben <3050760440@qq.com>
2021-12-12 22:15:03 +0800

    12-12

--- 5953subArrayRanges.cpp
+++ 5953subArrayRanges.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 class Solution {
 public:
-    long long subArrayRanges(vector<int>&nums) {
-
-    }
+	long long subArrayRanges(vector<int>& nums) {
+		long long ans = 0;
+		for (int i = 0; i < nums.size(); i++) {
+			int min = nums[i], max = nums[i];
+			for (int j = i + 1; j < nums.size(); j++) {
+				min = std::min(min, nums[j]);
+				max = std::max(max, nums[j]);
+				ans += (long long)max - min;
+			}
+		}
+		return ans;
+	}
 };
