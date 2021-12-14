31e131b2ffbcb191075df1eee9ae9ae3a7a01cf0
ben <3050760440@qq.com>
2021-12-14 11:37:44 +0800

    12-14

---
+++ 232.用栈实现队列.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=232 lang=cpp
+ *
+ * [232] 用栈实现队列
+ */
+#include<stack>
+// @lc code=start
+class MyQueue {
+    stack<int> m_stack;
+public:
+    MyQueue() {
+
+    }
+    
+    void push(int x) {
+
+    }
+    
+    int pop() {
+
+    }
+    
+    int peek() {
+
+    }
+    
+    bool empty() {
+
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
+// @lc code=end
+
