41e7ed4c374f9436bd0001e180c063f3f75b2e84
ben <3050760440@qq.com>
2021-12-15 16:45:34 +0800

    12-15

---
+++ 371.两整数之和.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=371 lang=cpp
+ *
+ * [371] 两整数之和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int getSum1(int a, int b) {//hhhhhh~
+        return a+b;
+    }
+};
+// @lc code=end
+
