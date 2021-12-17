8dbbe29804389fb9383790e6bb8d2e46811c7375
ben <3050760440@qq.com>
2021-12-17 11:16:05 +0800

    12-17

---
+++ 283.移动零.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=cpp
+ *
+ * [283] 移动零
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        int left=0,right=0;
+        while(right<nums.size()){
+            if(nums[right]){
+                swap(nums[left],nums[right]);
+                left++;
+            }
+            right++;
+        }
+        return;
+    }
+};
+// @lc code=end
