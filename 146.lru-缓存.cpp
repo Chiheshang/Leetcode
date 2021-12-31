e254463ea6b6190625fcf138330d25ee61c8404f
ben <3050760440@qq.com>
2021-12-31 17:01:38 +0800

    12-31

--- 146.lru-缓存.cpp
+++ 146.lru-缓存.cpp
@@ -18,29 +18,51 @@ public:
 
     int get(int key)
     {
+        if (index.find(key) == index.end())
+            return -1;
+        else
+        {
+            for (auto itor = index.begin(); itor != index.end(); itor++)
+            {
+                if (itor->first != key)
+                {
+                    itor->second[1]++;
+                }
+            }
+            index[key][1]=1;
+        }
         return index[key][0];
     }
 
     void put(int key, int value)
     {
-        if (index.size() < size)
+        if(index.size()==0){
+            index[key] = {value, 1};
+            return;
+        }
+        int temp = index.begin()->first;
+        int max = index.begin()->second[1];
+        for (auto itor = index.begin(); itor != index.end(); itor++)
+        {
+            // cout<<"value:"<<itor->first<<"time"<<itor->second[1]<<endl;
+            if (itor->second[1] > max)
+            {
+                max = itor->second[1];
+                temp = itor->first;
+            }
+            itor->second[1]++;
+        }
+        // cout<<"temp:"<<temp<<endl;
+        if (index.size() < size || index.find(key) != index.end())
         {
             index[key] = {value, 1};
         }
         else
         {
-            int temp=index.begin()->first;
-            int max=index[index.begin()->first][1];
-            for(auto itor:index){
-                if(itor.second[1]>max){
-                    max=itor.second[1];
-                    temp=itor.first;
-                }
-                itor.second[1]++;
-            }
-            index.erase(index.find(temp));
-            index[key]={value,1};
+            index.erase(temp);
+            index[key] = {value, 1};
         }
+        // cout<<index.size()<<endl;
     }
 };
 
