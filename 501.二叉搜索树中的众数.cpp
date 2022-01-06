a5deaaa873383bbb392685579e2010fa51e8ac43
ben <3050760440@qq.com>
2022-01-06 15:39:02 +0800

    1-6

--- 501.二叉搜索树中的众数.cpp
+++ 501.二叉搜索树中的众数.cpp
@@ -16,11 +16,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    vector<int> findMode(TreeNode* root) {
-
+    map<int, int> count;
+    void dfs(TreeNode *root)
+    {
+        if (root == nullptr)
+            return;
+        count[root->val]++;
+        if (root->left)
+            dfs(root->left);
+        if (root->right)
+            dfs(root->right);
+    }
+    vector<int> findMode(TreeNode *root)
+    {
+        count.clear();
+        vector<int> ans;
+        dfs(root);
+        int max = 0;
+        for (auto itor = count.begin(); itor != count.end(); itor++)
+        {
+            max = std::max(max, itor->second);
+        }
+        for (auto itor = count.begin(); itor != count.end(); itor++)
+        {
+            if (itor->second == max)
+            {
+                ans.push_back(itor->first);
+            }
+        }
+        return ans;
     }
 };
 // @lc code=end
-
