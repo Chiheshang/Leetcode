d29473fd8a35c1a6a0504922fc9ee28e6c59608e
ben <3050760440@qq.com>
2022-01-10 17:05:10 +0800

    1-10

---
+++ 172.阶乘后的零.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=172 lang=cpp
+ *
+ * [172] 阶乘后的零
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int trailingZeroes(int n)
+    {
+        int ans = 0;
+        while (n)
+        {
+            ans += n / 5;
+            n /= 5;
+        }
+        return ans;
+    }
+};
+// @lc code=end
